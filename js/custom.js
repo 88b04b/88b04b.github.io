e3346705c1d2cf14b213f9e1fe936dd102bbbe67
88b04b <110107051+88b04b@users.noreply.github.com>
2023-09-01 10:04:49 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+$(function () {
+    $('#fullpage').fullpage({
+        responsiveWidth: 768,
+        afterLoad: function (el, idx) {
+            setTimeout(function () {
+                $('.intro').addClass('animation');
+            }, 500);
+            if (idx >= 2) {
+                $('.section').eq(idx - 1).addClass('current').siblings().removeClass('current');
+            }
+        },
+        onLeave: function (index, nextIndex, direction) {
+            const page_title = ['INTRO', 'PROJECT 01', 'PROJECT 02', 'PROJECT 03', 'PROJECT 04', 'PROJECT 05', 'PROJECT 06', 'CONTACT']
+            $('nav ul li').eq(nextIndex - 1).addClass('active').siblings().removeClass('active');
+            $('.pagination').text(page_title[nextIndex - 1]);
+        },
+    });
+
+    $('.contents_btn').on('click', function () {
+        $(this).toggleClass('active');
+        $('aside').toggleClass('active');
+        $('.container').toggleClass('menu_active');
+
+        // if ($('aside').hasClass('active')) {
+        //     $('body').on('scroll touchmove mousewheel', function (event) {
+        //         event.preventDefault();
+        //         event.stopPropagation();
+        //         return false;
+        //     });
+        // } else {
+        //     $('body').off('scroll touchmove mousewheel');
+        // }
+    });
+
+    var sakura = new Sakura('.petals', {
+        fallSpeed: 2,
+        delay: 600,
+    });
+
+    $.fn.parallax = function (resistance, mouse) {
+        $el = $(this);
+        TweenLite.to($el, 0.2,
+            {
+                x: -((mouse.clientX - (window.innerWidth / 2)) / resistance),
+                y: -((mouse.clientY - (window.innerHeight / 2)) / resistance)
+            });
+    };
+
+    $(document).mousemove(function (e) {
+        $('.contact .circle').parallax(30, e);
+    });
+
+});
